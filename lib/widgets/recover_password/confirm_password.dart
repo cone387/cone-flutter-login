a9ef79773b1d1869175d5458fb4d60f2a692c8f2
18895379450 <fishlover387@gmail.com>
2021-11-29 18:19:18 +0800

    add code

---
+++ lib/widgets/recover_password/confirm_password.dart
@@ -0,0 +1,23 @@
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+
+class ConfirmPassordWidget extends StatefulWidget {
+  const ConfirmPassordWidget({ Key? key }) : super(key: key);
+
+  @override
+  _ConfirmPassordWidgetState createState() => _ConfirmPassordWidgetState();
+}
+
+class _ConfirmPassordWidgetState extends State<ConfirmPassordWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/widgets/recover_password/verify_code.dart
@@ -0,0 +1,50 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+class VerifyCodeWidget extends StatefulWidget {
+  const VerifyCodeWidget({ Key? key }) : super(key: key);
+
+  @override
+  _VerifyCodeWidgetState createState() => _VerifyCodeWidgetState();
+}
+
+class _VerifyCodeWidgetState extends State<VerifyCodeWidget> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return PinCodeTextField(
+      appContext: context,
+      length: 6,
+      obscureText: false,
+      animationType: AnimationType.fade,
+      pinTheme: PinTheme(
+        shape: PinCodeFieldShape.box,
+        borderRadius: BorderRadius.circular(5),
+        fieldHeight: 50,
+        fieldWidth: 40,
+        activeFillColor: Colors.white,
+      ),
+      animationDuration: Duration(milliseconds: 300),
+      backgroundColor: Colors.blue.shade50,
+      enableActiveFill: true,
+      // errorAnimationController: errorController,
+      // controller: textEditingController,
+      onCompleted: (v) {
+        print("Completed");
+      },
+      onChanged: (value) {
+        print(value);
+      },
+      beforeTextPaste: (text) {
+        print("Allowing to paste $text");
+        //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+        //but you can show anything you want here, like your pop up saying wrong paste format or etc
+        return true;
+      },
+    );
+  }
+}
